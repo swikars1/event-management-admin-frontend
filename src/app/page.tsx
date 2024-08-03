846097ee5da1c81f3f9385eb68f94f5bebd701b3
swikars1 <swikarsharma@gmail.com>
2024-08-04 02:35:02 +0545

    fix root route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
+"use client";
+
 import { MainTable } from "@/components/MainTable";
 import { Badge } from "@/components/ui/badge";
+import { commonService } from "@/services/common.service";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { data } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => {
+      return commonService.getAll("users");
+    },
+  });
+
+  const tableData = data?.responseObject?.map((a) => [
+    a.id,
+    a.name,
+    a.email,
+    <Badge variant="outline">User</Badge>,
+  ]);
+
   return (
-    <MainTable
-      caption="List of all users of our app."
-      title="Users"
-      headers={["ID", "Name", "Email", "Role", "Actions"]}
-      rows={[
-        ["1", "John Doe", "j@j.com", <Badge variant="outline">User</Badge>],
-        ["2", "Jane Doe", "j@j.com", <Badge variant="outline">User</Badge>],
-      ]}
-    />
+    <>
+      {tableData?.length > 0 ? (
+        <MainTable
+          caption="List of all users of our app."
+          title="Users"
+          headers={["ID", "Name", "Email", "Role", "Actions"]}
+          rows={tableData}
+        />
+      ) : null}
+    </>
   );
 }
